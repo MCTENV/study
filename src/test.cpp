e3fd8d2054d8d6b94e24c6bc7b0bf548926c6039
MakerFace <chenqi@imudges.com>
2022-09-06 19:05:09 +0800

    update

--- src/test.cpp
+++ src/test.cpp
@@ -5,35 +5,15 @@
 #include <vector>
 using namespace std;
 
-class A {
- private:
-  int n;
-  float f;
-  string s;
-
- public:
-  A() { cout << "zero" << endl; }
-  A(int n) : n(n) { cout << "i" << endl; }
-  A(int n, float f) : n(n), f(f) { cout << "if" << endl; }
-  A(int n, float f, string s) : n(n), f(f), s(s) { cout << "ifs" << endl; }
-};
-
-template <typename... Args>
-shared_ptr<A> create(Args... args) {
-  return shared_ptr<A>(new A(std::forward<Args>(args)...));
+#if __cplusplus == 201103L
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args &&...args) {
+  cout << "self make unique" << endl;
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
+#endif
 
-
-int main(int argc, char const* argv[]) {
-  vector<int> first({1, 2, 3, 4});
-  auto current = first;
-  do {
-    for (auto& i : first) {
-      cout << i << ' ';
-    }
-    cout << endl;
-    next_permutation(first.begin(), first.end());
-  } while (current != first);
-
+int main(int argc, char const *argv[]) {
+  auto upi = make_unique<int>(5);
   return 0;
 }
