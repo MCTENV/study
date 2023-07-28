ed0ecfd52c4bc459b5db793908e69ef0dbdcefea
MakerFace <chenqi@imudges.com>
2023-07-28 23:29:52 +0800

    objects

--- src/objects.cpp
+++ src/objects.cpp
@@ -1,24 +1,32 @@
 
-class Dopey {
+#include <cstdio>
+class X {
+ public:
+  virtual void foo() = 0;
+};
+class Dopey : public virtual X {
  public:
   Dopey() {}
+  void foo() override { printf("const char *__restrict format, ...\n"); }
 
  private:
   int a;
 };
 
-class Sneezy {
+class Sneezy : public virtual X {
  public:
   Sneezy() {}
   Sneezy(int b) : b(b) {}
+  void foo() override { printf("const char *__restrict format, ...\n"); }
 
  private:
   int b;
 };
 
-class Bashful {
+class Bashful : public virtual X {
  public:
   Bashful(){};
+  void foo() { printf("const char *__restrict format, ...\n"); }
 
  private:
   int c;
@@ -36,5 +44,6 @@ class SnowWhite {
 
 int main(int argc, char* argv[]) {
   SnowWhite snowWhite;
+  snowWhite.bashful.foo();
   return 0;
 }
